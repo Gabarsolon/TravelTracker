33484f194e36f4ca358e56279f6e8e063625415c
917tapoimarius <marius.tapoi@stud.ubbcluj.ro>
2023-11-12 21:04:36 +0200

    IDENTITY ids in DB
    I changed the DB tables a bit. I added IDENTITY property to the primary keys from User and Destination tables. This will autogenerate ids for elements which will be added in the tables

--- DB/dbScript.sql
+++ DB/dbScript.sql
@@ -1,26 +1,12 @@
--- Database: BucketListDB
-
--- DROP DATABASE IF EXISTS "BucketListDB";
-
-CREATE DATABASE "BucketListDB"
-    WITH
-    OWNER = postgres
-    ENCODING = 'UTF8'
-    LC_COLLATE = 'English_United States.1252'
-    LC_CTYPE = 'English_United States.1252'
-    TABLESPACE = pg_default
-    CONNECTION LIMIT = -1
-    IS_TEMPLATE = False;
-	
 CREATE TABLE "User" (
-   user_id INT PRIMARY KEY NOT NULL,
+   user_id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY NOT NULL,
 	email VARCHAR(100) NOT NULL,
 	username VARCHAR(50) NOT NULL,
 	password VARCHAR(50) NOT NULL
 );
 
 CREATE TABLE "Destination" (
-   destination_id INT PRIMARY KEY NOT NULL,
+   destination_id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY NOT NULL,
 	destination_country VARCHAR(50) NOT NULL,
 	destination_city VARCHAR(100) NOT NULL,
 	--destination_longitude DOUBLE NOT NULL, // to be added after first demo
@@ -34,15 +20,16 @@ CREATE TABLE "BucketList" (
    destination_id INT REFERENCES "Destination"(destination_id)
 );
 
-INSERT INTO "User" VALUES (1, 'user1@gmail.com', 'user1', 'user1')
+INSERT INTO "User"(email, username, password) VALUES ('user1@gmail.com', 'user1', 'user1')
 SELECT * FROM "User"
 
-INSERT INTO "Destination" VALUES
-(1, 'Romania', 'Bucharest', true),
-(2, 'Romania', 'Cluj-Napoca', true),
-(3, 'Japan', 'Tokyo', true),
-(4, 'Germany', 'Munich', true),
-(5, 'USA', 'Los Angeles', true)
+INSERT INTO "Destination" (destination_country, destination_city, is_public)
+VALUES
+('Romania', 'Bucharest', true),
+('Romania', 'Cluj-Napoca', true),
+('Japan', 'Tokyo', true),
+('Germany', 'Munich', true),
+('USA', 'Los Angeles', true)
 SELECT * FROM "Destination"
 
 INSERT INTO "BucketList" VALUES
