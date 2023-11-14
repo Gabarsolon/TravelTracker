1fe78454c2052872bfb3f462eb0ab0c5346249b9
DayanaRalucaMardari <dayana.mardari@stud.ubbcluj.ro>
2023-11-14 22:57:54 +0200

    Add validation to DestinationRequest fields

--- backend/src/main/java/com/bucketlist/chatGPT/model/DestinationRequest.java
+++ backend/src/main/java/com/bucketlist/chatGPT/model/DestinationRequest.java
@@ -1,20 +1,40 @@
 package com.bucketlist.chatGPT.model;
 
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
 import lombok.Data;
-import lombok.NonNull;
 
 @Data
 public class DestinationRequest {
-    @NonNull
+    @NotNull(message = "The CONTINENT field cannot be null.")
+    @NotEmpty(message = "The CONTINENT field cannot be empty.")
+    @Pattern(regexp="^[A-Za-z]*$", message = "The CONTINENT field must contain only letters.")
     private String continent;
-    @NonNull
+
+    @NotNull(message = "The COUNTRY field cannot be null.")
+    @NotEmpty(message = "The COUNTRY field cannot be empty.")
+    @Pattern(regexp="^[A-Za-z]*$", message = "The COUNTRY field must contain only letters.")
     private String country;
-    @NonNull
+
+    @NotNull(message = "The REGION TYPE field cannot be null.")
+    @NotEmpty(message = "The REGION TYPE field cannot be empty.")
+    @Pattern(regexp="^[A-Za-z]*$", message = "The REGION TYPE field must contain only letters.")
     private String regionType;
-    @NonNull
+
+    @NotNull(message = "The MONTH field cannot be null.")
+    @NotEmpty(message = "The MONTH field cannot be empty.")
+    @Pattern(regexp="^[A-Za-z]*$", message = "The MONTH field must contain only letters.")
     private String month;
-    @NonNull
+
+    @NotNull(message = "The SEASON field cannot be null.")
+    @NotEmpty(message = "The SEASON field cannot be empty.")
+    @Pattern(regexp="^((in|off)-season)|anytime", message = "regex")
+    //@Pattern(regexp="^[A-Za-z]*$", message = "The SEASON field must contain only letters.")
     private String season;
-    @NonNull
+
+    @NotNull(message = "The ACTIVITIES field cannot be null.")
+    @NotEmpty(message = "The ACTIVITIES field cannot be empty.")
+    @Pattern(regexp = "^[A-Za-z]*(, *[A-Za-z]+)*", message = "Invalid activities. Please enumerate the activities with commas in between them.")
     private String activities;
 }
