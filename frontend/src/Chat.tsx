c09cb25d49efda9af2c924c850448d9ee68d1bff
914LupeanOana <91674387+914LupeanOana@users.noreply.github.com>
2023-11-14 19:00:26 +0200

    layout and chat

---
+++ frontend/src/Chat.tsx
@@ -0,0 +1,14 @@
+import './Chat.css'; 
+
+const Chat: React.FC = () => {
+    return (      
+        <div className="title">
+            <p>Talk with the TravelBot</p>
+            <div className="description">
+                <p><em>Hello, I'm at your disposal to provide you the best destinations! Tell me what you would like..</em></p>
+            </div>
+        </div>     
+    );
+  };
+
+export default Chat;
