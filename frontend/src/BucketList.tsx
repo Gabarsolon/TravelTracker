6fa4376f6cdca1d1bc6123b6b8ce5b8b2e06333f
914LolutaStefana <stefanal002@yahoo.com>
2023-11-17 18:49:17 +0200

    Add is done!

--- frontend/src/BucketList.tsx
+++ frontend/src/BucketList.tsx
@@ -1,149 +1,66 @@
-
-// import React, { useState, useEffect } from 'react';
-
-// interface Destination {
-//   destinationCountry: string;
-//   destinationCity: string;
-//   public: boolean;
-// }
-
-// const BucketList: React.FC = () => {
-//   const [bucketList, setBucketList] = useState<Destination[]>([]);
-//   const [showAddModal, setShowAddModal] = useState<boolean>(false);
-//   const [newDestination, setNewDestination] = useState<Destination>({
-//     destinationCountry: '',
-//     destinationCity: '',
-//     public: false,
-//   });
-
-//   // Hardcoded destinations for demonstration
-//   const hardcodedDestinations: Destination[] = [
-//     { destinationCountry: 'Italy', destinationCity: 'Rome', public: false },
-//     { destinationCountry: 'Australia', destinationCity: 'Sydney', public: false },
-//     { destinationCountry: 'Canada', destinationCity: 'Vancouver', public: false },
-//   ];
-
-//   useEffect(() => {
-//     // Use the hardcoded destinations initially
-//     setBucketList(hardcodedDestinations);
-//   }, []);
-// //   useEffect(() => {
-// //        fetch('http://localhost:8080/api/v1/destination/destinationsInBucketList/1', {
-// //           method: 'GET',
-// //           headers: {
-// //             'Content-Type': 'application/json',
-// //           },
-// //         })
-// //           .then(response => response.json())
-// //           .then((data: Destination[]) => setBucketList(data))
-// //           .catch(error => console.error('Error fetching bucket list:', error));
-// //       }, []);
-
-//   const handleAddDestination = () => {
-//     setBucketList(prevList => [...prevList, newDestination]);
-//     setShowAddModal(false);
-//     // Clear the form after adding a destination
-//     setNewDestination({
-//       destinationCountry: '',
-//       destinationCity: '',
-//       public: false,
-//     });
-//   };
-
-//   return (
-//     <div>
-//       <h2 className='titleOfList'>Bucket List</h2>
-//       <div className="list-container">
-//         {bucketList.length === 0 ? (
-//           <p>Empty Bucket List</p>
-//         ) : (
-//           <ul>
-//             {bucketList.map((destination, index) => (
-//               <li key={index}>
-//                 {destination.destinationCountry}, {destination.destinationCity}
-//               </li>
-//             ))}
-//           </ul>
-//         )}
-//       </div>
-//       <button className="addButton" onClick={() => setShowAddModal(true)}>
-//         +
-//       </button>
-      
-
-//       {/* Add destination modal */}
-//       {showAddModal && <div className="overlay" />}
-//       {showAddModal && (
-//         <div className="modal">
-//           <label>
-//             Country:
-//             <input
-//               type="text"
-//               value={newDestination.destinationCountry}
-//               onChange={e => setNewDestination({ ...newDestination, destinationCountry: e.target.value })}
-//             />
-//           </label>
-//           <label>
-//             City:
-//             <input
-//               type="text"
-//               value={newDestination.destinationCity}
-//               onChange={e => setNewDestination({ ...newDestination, destinationCity: e.target.value })}
-//             />
-//           </label>
-//           <button onClick={handleAddDestination}>Add Destination</button>
-//           <button onClick={() => setShowAddModal(false)}>Cancel</button>
-//         </div>
-//       )}
-//     </div>
-//   );
-// };
-
-// export default BucketList;
 import React, { useState, useEffect } from 'react';
 
 interface Destination {
-    destinationName: string;
-    destinationCountry: string;
-    destinationCity: string;
-    public: boolean;
-    description: string;
+  
+  destinationCountry: string;
+  destinationCity: string;
+  public: boolean;
+  description: string;
+  destinationName: string;
 }
 
 const BucketList: React.FC = () => {
   const [bucketList, setBucketList] = useState<Destination[]>([]);
   const [showAddModal, setShowAddModal] = useState<boolean>(false);
   const [newDestination, setNewDestination] = useState<Destination>({
-    destinationName: '',
     destinationCountry: '',
     destinationCity: '',
     public: false,
     description: '',
+    destinationName: '',
   });
 
-  // Hardcoded destinations for demonstration
-  const hardcodedDestinations: Destination[] = [
-    { destinationName: 'LALA', destinationCountry: 'Italy', destinationCity: 'Rome', public: false, description: 'LALA' },
-    { destinationName: 'LALA', destinationCountry: 'Italy', destinationCity: 'Rome', public: false, description: 'LALA'},
-   // { destinationCountry: 'Canada', destinationCity: 'Vancouver', public: false },
-  ];
-
   useEffect(() => {
-    // Use the hardcoded destinations initially
-    setBucketList(hardcodedDestinations);
+    fetch('http://localhost:8080/api/v1/destination/destinationsInBucketList/1', {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    })
+      .then(response => response.json())
+      .then((data: Destination[]) => setBucketList(data))
+      .catch(error => console.error('Error fetching bucket list:', error));
   }, []);
 
-  const handleAddDestination = () => {
-    setBucketList(prevList => [...prevList, newDestination]);
-    setShowAddModal(false);
-    // Clear the form after adding a destination
-    setNewDestination({
-        destinationName: '',
-      destinationCountry: '',
-      destinationCity: '',
-      public: false,
-        description: '',
-    });
+  const handleAddDestination = async () => {
+    try {
+      const response = await fetch('http://localhost:8080/api/v1/destination/add/1', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(newDestination),
+      });
+
+      if (response.ok) {
+        // Add the new destination to the bucket list
+        setBucketList(prevList => [...prevList, newDestination]);
+        setShowAddModal(false);
+        // Clear the form after adding a destination
+        setNewDestination({
+          
+          destinationCountry: '',
+          destinationCity: '',
+          public: false,
+          description: '',
+          destinationName: '',
+        });
+      } else {
+        console.error('Error adding destination:', response.statusText);
+      }
+    } catch (error) {
+      console.error('Error adding destination:', error);
+    }
   };
 
   return (
@@ -156,7 +73,7 @@ const BucketList: React.FC = () => {
           <ul>
             {bucketList.map((destination, index) => (
               <li key={index}>
-                {destination.destinationCountry}, {destination.destinationCity}
+                {destination.destinationName}, {destination.destinationCountry}, {destination.destinationCity}
               </li>
             ))}
           </ul>
@@ -166,33 +83,49 @@ const BucketList: React.FC = () => {
         +
       </button>
 
-      {/* Add destination modal */}
-    
       {showAddModal && (
-        <div className="modal">
-          <label>
-            Country:
-            <input
-              type="text"
-              value={newDestination.destinationCountry}
-              onChange={e => setNewDestination({ ...newDestination, destinationCountry: e.target.value })}
-            />
-          </label>
-          <label>
-            City:
-            <input
-              type="text"
-              value={newDestination.destinationCity}
-              onChange={e => setNewDestination({ ...newDestination, destinationCity: e.target.value })}
-            />
-          </label>
-          <button onClick={handleAddDestination}>Add Destination</button>
-          <button onClick={() => setShowAddModal(false)}>Cancel</button>
-        </div>
+        <>
+          <div className="overlay" />
+          <div className="modal">
+            <h4 className='Addtitle'>Add destination</h4>
+            <label>
+              Name:
+              <input
+                type="text"
+                value={newDestination.destinationName}
+                onChange={e => setNewDestination({ ...newDestination, destinationName: e.target.value })}
+              />
+            </label>
+            <label>
+              Country:
+              <input
+                type="text"
+                value={newDestination.destinationCountry}
+                onChange={e => setNewDestination({ ...newDestination, destinationCountry: e.target.value })}
+              />
+            </label>
+            <label>
+              City:
+              <input
+                type="text"
+                value={newDestination.destinationCity}
+                onChange={e => setNewDestination({ ...newDestination, destinationCity: e.target.value })}
+              />
+            </label>
+            <label>
+              Description:
+              <input
+                type="text"
+                value={newDestination.description}
+                onChange={e => setNewDestination({ ...newDestination, description: e.target.value })}
+              />
+            </label>
+            <button onClick={handleAddDestination}>Add Destination</button>
+            <button onClick={() => setShowAddModal(false)}>Cancel</button>
+          </div>
+        </>
       )}
     </div>
   );
 };
-
 export default BucketList;
-
