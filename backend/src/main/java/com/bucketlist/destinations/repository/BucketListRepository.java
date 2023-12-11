7b07472952cd8522716acf666495b7e55252898e
917tapoimarius <marius.tapoi@stud.ubbcluj.ro>
2023-12-11 21:17:03 +0200

    BS-20: added filtering functionality

--- backend/src/main/java/com/bucketlist/destinations/repository/BucketListRepository.java
+++ backend/src/main/java/com/bucketlist/destinations/repository/BucketListRepository.java
@@ -2,7 +2,9 @@ package com.bucketlist.destinations.repository;
 
 import java.util.List;
 import com.bucketlist.destinations.model.BucketList;
+import com.bucketlist.destinations.model.Destination;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface BucketListRepository extends JpaRepository<BucketList, Long> {
 
--- backend/src/main/java/com/bucketlist/destinations/repository/DestinationRepository.java
+++ backend/src/main/java/com/bucketlist/destinations/repository/DestinationRepository.java
@@ -5,7 +5,23 @@ import com.bucketlist.destinations.model.Destination;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface DestinationRepository extends JpaRepository<Destination, Long> {
      List<Destination> findDestinationsByIsPublic(boolean isPublic);
+
+     List<Destination> findDestinationByIsPublicAndDestinationCityContainingIgnoreCase(boolean isPublic, String destinationCity);
+
+     List<Destination> findDestinationByIsPublicAndDestinationNameContainingIgnoreCase(boolean isPublic, String destinationName);
+
+     List<Destination> findDestinationByIsPublicAndDestinationCountryContainingIgnoreCase(boolean isPublic, String destinationCountry);
+
+     @Query(value = "SELECT d.destination_id, d.destination_country, d.destination_city, d.is_public, d.description, d.destination_name\n" +
+             "FROM \"Destination\" d\n" +
+             "INNER JOIN \"BucketList\" bl ON d.destination_id = bl.destination_id\n" +
+             "WHERE bl.user_id = ?1\n" +
+             "AND (d.destination_country ILIKE %?2% \n" +
+             "AND d.destination_city ILIKE %?3%\n" +
+             "AND d.destination_name ILIKE %?4%);", nativeQuery = true)
+     List<Destination> findDestinationsForGivenUserId(Long userId, String country, String city, String name);
 }
