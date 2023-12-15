7efd0848a24b8604bcb4afbfb2c6101649fbc45a
914LolutaStefana <stefanal002@yahoo.com>
2023-12-15 16:43:48 +0200

    .

--- frontend/src/BucketList.tsx
+++ frontend/src/BucketList.tsx
@@ -1,7 +1,6 @@
 import React, { useState, useEffect } from 'react';
 
 interface Destination {
-  
   destinationCountry: string;
   destinationCity: string;
   public: boolean;
@@ -19,18 +18,34 @@ const BucketList: React.FC = () => {
     description: '',
     destinationName: '',
   });
+  const [searchTerm, setSearchTerm] = useState<string>('');
+  const [filterAttribute, setFilterAttribute] = useState<string>('DestinationName');
 
   useEffect(() => {
-    fetch('http://localhost:8080/api/v1/destination/destinationsInBucketList/1', {
-      method: 'GET',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-    })
-      .then(response => response.json())
-      .then((data: Destination[]) => setBucketList(data))
-      .catch(error => console.error('Error fetching bucket list:', error));
-  }, []);
+    const fetchData = async () => {
+      try {
+        const userId = 1; // Replace with the appropriate user ID
+
+        const response = await fetch(`http://localhost:8080/api/v1/destination/destinationsInBucketList/${userId}?pageNumber=0&pageSize=100&filteringAttribute=${filterAttribute}&filterInputData=${searchTerm}`, {
+          method: 'GET',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+        });
+
+        if (response.ok) {
+          const data = await response.json();
+          setBucketList(data);
+        } else {
+          console.error('Error fetching bucket list:', response.statusText);
+        }
+      } catch (error) {
+        console.error('Error fetching bucket list:', error);
+      }
+    };
+
+    fetchData();
+  }, [searchTerm, filterAttribute]);
 
   const handleAddDestination = async () => {
     try {
@@ -48,7 +63,6 @@ const BucketList: React.FC = () => {
         setShowAddModal(false);
         // Clear the form after adding a destination
         setNewDestination({
-          
           destinationCountry: '',
           destinationCity: '',
           public: false,
@@ -63,17 +77,43 @@ const BucketList: React.FC = () => {
     }
   };
 
+  const handleSearchChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setSearchTerm(event.target.value);
+  };
+
+  const handleFilterChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
+    setFilterAttribute(event.target.value);
+  };
+
   return (
     <div>
       <h2 className='titleOfList'>Bucket List</h2>
       <div className="list-container">
+        <div className="filter-bar">
+          <input
+            type="text"
+            placeholder="Search..."
+            value={searchTerm}
+            onChange={handleSearchChange}
+          />
+          <select value={filterAttribute} onChange={handleFilterChange}>
+            <option value="DestinationName">Name</option>
+            <option value="DestinationCity">City</option>
+            <option value="DestinationCountry">Country</option>
+          </select>
+        </div>
         {bucketList.length === 0 ? (
           <p>Empty Bucket List</p>
         ) : (
           <ul>
             {bucketList.map((destination, index) => (
               <li key={index}>
-                {destination.destinationName}, {destination.destinationCountry}, {destination.destinationCity}
+                <strong style={{ fontSize: '1.2em', fontStyle: 'oblique' }}>{destination.destinationName}</strong><br />
+                {destination.destinationCity}, {destination.destinationCountry}<br />
+                <i style={{ fontSize: '0.8em' }}>
+                  {destination.description.length > 50 ? `${destination.description.slice(0, 50)}...` : destination.description}
+                </i>
+                <li></li>
               </li>
             ))}
           </ul>
@@ -128,4 +168,5 @@ const BucketList: React.FC = () => {
     </div>
   );
 };
+
 export default BucketList;
--- frontend/src/PublicList.tsx
+++ frontend/src/PublicList.tsx
@@ -1,44 +1,100 @@
 import React, { useEffect, useState } from 'react';
 
 interface Destination {
-  destinationName: string;
+  destinationId: number;
   destinationCountry: string;
   destinationCity: string;
-  public: boolean;
+  destinationName: string;
   description: string;
+  public: boolean;
 }
 
 const PublicList: React.FC = () => {
   const [publicList, setPublicList] = useState<Destination[]>([]);
+  const [searchTerm, setSearchTerm] = useState<string>('');
+  const [filterAttribute, setFilterAttribute] = useState<string>('DestinationName');
 
   useEffect(() => {
-    fetch('http://localhost:8080/api/v1/destination/allDestinations', {
-      method: 'GET',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-    })
-      .then(response => response.json())
-      .then((data: Destination[]) => setPublicList(data))
-      .catch(error => console.error('Error fetching public list:', error));
-  }, []);
+    const fetchFilteredDestinations = async () => {
+      try {
+        const params = new URLSearchParams();
+        params.append('filteringAttribute', filterAttribute);
+        params.append('filterInputData', searchTerm || '');
+        params.append('pageNumber', '0');  // Set your desired page number
+        params.append('pageSize', '100');    // Set your desired page size
+
+        const filterUrl = `http://localhost:8080/api/v1/destination/filterPublicDestinations?${params.toString()}`;
+
+        const response = await fetch(filterUrl, {
+          method: 'GET',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+        });
+
+        if (response.ok) {
+          const data: Destination[] = await response.json();
+          setPublicList(data);
+        } else {
+          console.error('Error fetching public list:', response.statusText);
+        }
+      } catch (error) {
+        console.error('Error fetching public list:', error);
+      }
+    };
+
+    fetchFilteredDestinations();
+  }, [searchTerm, filterAttribute]);
+
+  const displayedDestinations = publicList;
+
+  const handleSearchChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setSearchTerm(event.target.value);
+  };
+
+  const handleFilterChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
+    setFilterAttribute(event.target.value);
+  };
 
   return (
-    <div >
+    <div>
+      
       <h2 className='titleOfList'>Public List</h2>
+      
       <div className="list-container">
-      {publicList.length === 0 ? (
-        <p>Empty Public List </p>
-      ) : (
-        <ul>
-          {publicList.map((destination, index) => (
-            <li key={index}>
-              {destination.destinationCountry}, {destination.destinationCity}
-            </li>
-          ))}
-        </ul>
-      )}
-    </div>
+      <div className="filter-bar">
+        <input
+          type="text"
+          placeholder="Search..."
+          value={searchTerm}
+          onChange={handleSearchChange}
+        />
+        <select value={filterAttribute} onChange={handleFilterChange}>
+          <option value="DestinationName">Name</option>
+          <option value="DestinationCity">City</option>
+          <option value="DestinationCountry">Country</option>
+        </select>
+      </div>
+        {displayedDestinations.length === 0 ? (
+          <p>Empty Public List</p>
+        ) : (
+          <ul>
+            {displayedDestinations.map((destination) => (
+              <li key={destination.destinationId}>
+                <strong style={{ fontSize: '1.2em', fontStyle: 'oblique' }}>
+                  {destination.destinationName}
+                </strong><br />
+                {destination.destinationCity}, {destination.destinationCountry}<br />
+                <i style={{ fontSize: '0.8em' }}>
+                  {destination.description.length > 50
+                    ? `${destination.description.slice(0, 50)}...`
+                    : destination.description}
+                </i>
+              </li>
+            ))}
+          </ul>
+        )}
+      </div>
     </div>
   );
 };
