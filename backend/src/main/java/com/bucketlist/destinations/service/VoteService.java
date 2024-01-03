7b7be8e1ec264531a659f973b5d2f35ea27499ba
DayanaRalucaMardari <dayana.mardari@stud.ubbcluj.ro>
2024-01-03 22:33:44 +0200

    Add user voting functionality

--- backend/src/main/java/com/bucketlist/destinations/service/VoteService.java
+++ backend/src/main/java/com/bucketlist/destinations/service/VoteService.java
@@ -1,11 +1,14 @@
 package com.bucketlist.destinations.service;
 
+import com.bucketlist.destinations.exception.ResourceNotFoundException;
+import com.bucketlist.destinations.model.Destination;
 import com.bucketlist.destinations.model.Vote;
 import com.bucketlist.destinations.repository.VoteRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class VoteService {
@@ -29,5 +32,26 @@ public class VoteService {
 
     public List<Vote> getAllDestinationVotes(Long destinationId) {
         return voteRepository.findDestinationByDestinationId(destinationId);
+    }
+
+    public Vote getVoteByDestinationIdAndMonth(Long destinationId, Long month) {
+        Vote vote = voteRepository.findVoteByDestinationIdAndMonth(destinationId, month);
+        if (vote != null) {
+            return vote;
+        } else {
+            throw new UnsupportedOperationException("Vote number cannot be increased. No such vote with the given destinationId and month.");
+        }
+    }
+
+    public Vote updateVoteNumber(Long voteId) {
+        Optional<Vote> existingVote = voteRepository.findById(voteId);
+        if (existingVote.isPresent()) {
+            Vote vote = existingVote.get();
+            int currentNumberOfVotes = vote.getNumber();
+            vote.setNumber(currentNumberOfVotes + 1);
+            return this.voteRepository.save(vote);
+        } else {
+            throw new UnsupportedOperationException("Vote number cannot be increased. Vote not found.");
+        }
     }
 }
